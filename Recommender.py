442e5dd734d58fdf803e439ca0221a07d2a76b72
Keshav Raj <www.keshav.mars@gmail.com>
2022-07-02 02:10:24 +0530

    Recommender (Static) done

--- Recommender.py
+++ Recommender.py
@@ -1,21 +1,26 @@
+from ast import Return
 import pandas as pd
 import numpy as np
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# song_df, user_df, con_df = None, None, None
+song_df, user_df, con_df = None, None, None
 
-# def load_dataset():
-song_df = pd.read_csv('dataset/song_dataset.csv', dtype={"year": "string"})
-user_df = pd.read_csv('dataset/user_dataset.csv')
-song_df.drop_duplicates(subset='song_id', keep='first', inplace=True)
+def load_dataset():
+    global song_df, user_df, con_df
+    song_df = pd.read_csv('dataset/song_dataset.csv', dtype={"year": "string"})
+    user_df = pd.read_csv('dataset/user_dataset.csv')
+    song_df.drop_duplicates(subset='song_id', keep='first', inplace=True)
 
-con_df = pd.merge(user_df, song_df, on='song_id', how='left')
-con_df['song'] = con_df['title'] + ' - ' + con_df['artist_name']
-con_df = con_df.head(10000)
+    con_df = pd.merge(user_df, song_df, on='song_id', how='left')
+    con_df['song'] = con_df['title'] + ' - ' + con_df['artist_name']
+    con_df = con_df.head(10000)
 
 
 def popular(n=10):
-    con_df = pd.merge(user_df, song_df, on='song_id', how='left')
-    con_df['song'] = con_df['title'] + ' - ' + con_df['artist_name']
+    # con_df = pd.merge(user_df, song_df, on='song_id', how='left')
+    # con_df['song'] = con_df['title'] + ' - ' + con_df['artist_name']
     song_grouped = con_df.groupby(['song']).agg(
         {'listen_count': 'count'}).reset_index()
     grouped_sum = song_grouped['listen_count'].sum()
@@ -37,8 +42,9 @@ def get_songs_of_user(user):
 
 
 def collaborative(user, n=10):
-    if type(user) == 'list':
+    if type(user) == type([]):
         user_songs = user
+        user = ''
     else:
         user_songs = list(get_songs_of_user(user))
     all_songs = list(con_df['song'].unique())
@@ -70,7 +76,49 @@ def collaborative(user, n=10):
 
     return df if df.shape[0] != 0 else 0
 
+def content_similarity(songs, n=10):
+    song_features = song_df['title'] + " " + song_df['artist_name'] + " " + song_df['release']
+    vectorizer = TfidfVectorizer()
+    song_features = vectorizer.fit_transform(song_features.values.astype('U'))
+    similarity = []
+    for song in songs:
+         
+        similarity.append(list(cosine_similarity(
+            song_features[song_df[song_df['song_id'] == song].index[0]], song_features)[0]))
+    # print(len(similarity), 'x', len(similarity[0]))
+
+    similarity_score = []
+
+    for i in range(len(songs)):
+        for index, score in enumerate(similarity[i]):
+            similarity_score.append((index, score))
+
+    similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    recommend_song, i, j = [similarity_score[0][0]], 0, 1
+    while i < n + len(songs) - 1:
+        if similarity_score[j][0] != recommend_song[i]:
+            recommend_song.append(similarity_score[j][0])
+            i += 1
+        j += 1
+    # return recommend_song[len(songs):]
+    for i, song in enumerate(recommend_song):
+        recommend_song[i] = song_df.iloc[song]['title']
+
+    # return song_df[song_df['song_id'] in recommend_song]
+    return recommend_song
+        
+def user_content_similarity(user_id, n=10):
+    user_data = user_df[user_df['user_id'] == user_id]
+    user_data = user_data.sort_values('listen_count', ascending=False)
+    return content_similarity(list(user_data['song_id'].head(10)), n)
 
 if __name__ == '__main__':
-    # load_dataset()
-    print(collaborative(user_df['user_id'][100]))
+    load_dataset()
+    # print(collaborative(user_df['user_id'][100]))
+    # df = collaborative(['Where Did You Sleep Last Night - Nirvana',
+    #       'The Day That Never Comes - Metallica'])
+    # print(df)
+    # df.to_json('fav.json')
+    print(content_similarity(['SOBXHDL12A81C204C0', 'SOAKIMP12A8C130995']))
+    # print(user_content_similarity(user_df['user_id'][100]))
+    
